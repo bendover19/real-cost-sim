74eeddfe06f75a89401cf553919aefbc2ba567e2
bendover19 <98707954+bendover19@users.noreply.github.com>
2025-11-24 22:14:56 +0000

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,17 @@
+// next.config.ts
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
+const nextConfig = {
   async rewrites() {
     return [
       {
+        // pretty URL:  /enough/uk/london/28000
         source: "/enough/:country/:city/:salary",
+        // internal URL: /enough?country=uk&city=london&salary=28000
         destination: "/enough?country=:country&city=:city&salary=:salary",
       },
     ];
   },
-};
+} satisfies NextConfig;
 
 export default nextConfig;
